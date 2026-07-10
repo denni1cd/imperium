e3a6c1b69f73dc49ae092700ce505f29a68168fd
denni1cd <131931760+denni1cd@users.noreply.github.com>
2026-07-10 14:09:04 -0500

    Add Imperium package

---
+++ src/imperium/__init__.py
@@ -0,0 +1,19 @@
+"""Imperium strategic deliberation engine."""
+
+from imperium.domain.models import (
+    ActionablePlan,
+    DeliberationRecord,
+    MemberProfile,
+    SovereignRequest,
+    ValueVector,
+)
+
+__all__ = [
+    "ActionablePlan",
+    "DeliberationRecord",
+    "MemberProfile",
+    "SovereignRequest",
+    "ValueVector",
+]
+
+__version__ = "0.1.0"
